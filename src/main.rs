aa9ef9680462d15feb0e1758a6719851ef5654d4
Moritz Eigenauer <moritz.eigenauer@gmail.com>
2025-11-20 11:11:25 +0100

    remove unused mut

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use chess_engine::engine::Engine;
 use chess_engine::uci::UCI;
 
 fn main() {
-    let mut engine = Engine::new("Yakari".to_string(), "EiSiMo".to_string());
+    let engine = Engine::new("Yakari".to_string(), "EiSiMo".to_string());
     let mut uci = UCI { engine };
     uci.uci_mainloop();
 }
